ca0df689cd36d57d3480b8e8c92c4b39a11421c4
FirasArafah <43936321+FirasArafah@users.noreply.github.com>
2019-03-06 21:30:06 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,8 +9,8 @@ client.on('ready', () => {
 
 
 
-
-hero.on('message',async message => {
+var prefix = "!";
+client.on('message',async message => {
   if(message.author.bot || message.channel.type === 'dm') return;
   if(!message.content.startsWith(prefix)) return;
   let cmd = message.content.split(" ")[0].substring(prefix.length);
@@ -21,7 +21,7 @@ hero.on('message',async message => {
   if(cmd === 'clear') {
     if(!devs.includes(message.author.id)) return message.channel.send(`**${err} You are not one of the bot admins.**`);
     let fetched = await message.channel.fetchMessages();
-    let filtered = await fetched.filter(r => r.author.id === hero.user.id);
+    let filtered = await fetched.filter(r => r.author.id === client.user.id);
     if(filtered.size <= 0) return message.channel.send(`**${err} There are no messages to delete.**`).then(m => m.delete(5000));
     message.channel.bulkDelete(filtered)
     .then(() => {
